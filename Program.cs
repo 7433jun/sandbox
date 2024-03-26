5b97f0bba2c843aac84ed193bb431f14aef8298f
7433jun <7433jun@naver.com>
2024-03-26 14:51:24 +0900

    20240326

--- Program.cs
+++ Program.cs
@@ -4,55 +4,19 @@ namespace Test
 {
     public class Program
     {
-        public static void Main()
+
+        static void Print<T>(params T[] args)
         {
-            int N = int.Parse(Console.ReadLine());
-            int[] ints = new int[N];
-
-            for (int i = 0; i < N; i++)
+            foreach(var e in args)
             {
-                ints[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine(e);
             }
+        }
 
-            double avg = Math.Round((double)ints.Sum() / (double)ints.Count());
-
-            
-
-            Console.WriteLine((int)avg);
-
-            Array.Sort(ints);
-
-            Console.WriteLine(ints[ints.Count() / 2]);
-
-            int[] nums = new int[8001];
-
-            for (int i = 0; i < ints.Count(); i++)
-            {
-                nums[ints[i] + 4000]++;
-            }
-
-            int max = nums.Max();
-
-            List<int> list = new List<int>();
-
-            for (int i = 0; i < nums.Count(); i++)
-            {
-                if (max == nums[i])
-                {
-                    list.Add(i - 4000);
-                }
-            }
-
-            if (list.Count() == 1)
-            {
-                Console.WriteLine(list[0]);
-            }
-            else
-            {
-                Console.WriteLine(list[1]);
-            }
-
-            Console.WriteLine(ints[ints.Count() - 1] - ints[0]);
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Program.Print(1, 'a', 3.14);
         }
     }
 }
